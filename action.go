96bba0a8e5d500f2eba905c5f5022fdb8cc31f5c
Rostyslav Popadyn <rostykpopadynrostykpopadyn451@gmail.com>
2021-11-05 15:58:01 +0100

    added host

--- action.go
+++ action.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "fmt"
+    "github.com/brunoa19/shipa-github-actions/shipa"
     "os"
 )
 
@@ -14,6 +15,13 @@ func main() {
         fmt.Println("token:", token)
     }
 
+    _, err := shipa.New()
+    if err != nil {
+        fmt.Println("ERR failed create shipa client:", err)
+    } else {
+        fmt.Println("shipa client created")
+    }
+
     args := os.Args[1:]
     fmt.Println("Input args:", args)
     if len(args) > 0 {
@@ -21,9 +29,17 @@ func main() {
         fmt.Println("Input file path:", path)
 
         if _, err := os.Stat(path); err != nil {
-            fmt.Println("ERR:", err)
+            fmt.Println("ERR file stats:", err)
         } else {
             fmt.Println("File exists: OK")
+
+            rawContent, err := os.ReadFile(path)
+            if err != nil {
+                fmt.Println("ERR read file failed:", err)
+            } else {
+                fmt.Println(string(rawContent))
+            }
+
         }
     }
 }
