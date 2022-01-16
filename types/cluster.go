fcbd248b3a0fc0520b3fca2a61b04ac921c9471e
Rostyslav Popadyn <rostykpopadynrostykpopadyn451@gmail.com>
2022-01-16 12:03:57 +0100

    fixed loading token/cert from file - trim spaces/new lines/tabs

--- types/cluster.go
+++ types/cluster.go
@@ -1,114 +1,116 @@
 package types
 
 import (
-    "encoding/json"
-    "fmt"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+    "strings"
+
     "github.com/brunoa19/shipa-github-actions/shipa"
-    "io/ioutil"
-    "os"
 )
 
 type Cluster struct {
-    Name      string            `json:"name" yaml:"name"`
-    Endpoint  *ClusterEndpoint  `json:"endpoint" yaml:"endpoint"`
-    Resources *ClusterResources `json:"resources,omitempty" yaml:"resources,omitempty"`
+	Name      string            `json:"name" yaml:"name"`
+	Endpoint  *ClusterEndpoint  `json:"endpoint" yaml:"endpoint"`
+	Resources *ClusterResources `json:"resources,omitempty" yaml:"resources,omitempty"`
 }
 
 func (c *Cluster) ToShipaCluster() (*shipa.Cluster, error) {
-    var frameworks []*shipa.Framework
-    if c.Resources != nil && c.Resources.Frameworks != nil {
-        for _, name := range c.Resources.Frameworks.Name {
-            frameworks = append(frameworks, &shipa.Framework{
-                Name: name,
-            })
-        }
-        c.Resources.Frameworks = nil
-    }
+	var frameworks []*shipa.Framework
+	if c.Resources != nil && c.Resources.Frameworks != nil {
+		for _, name := range c.Resources.Frameworks.Name {
+			frameworks = append(frameworks, &shipa.Framework{
+				Name: name,
+			})
+		}
+		c.Resources.Frameworks = nil
+	}
 
-    rawJson, err := json.Marshal(c)
-    if err != nil {
-        return nil, err
-    }
+	rawJson, err := json.Marshal(c)
+	if err != nil {
+		return nil, err
+	}
 
-    cluster := &shipa.Cluster{}
-    err = json.Unmarshal(rawJson, cluster)
-    if err != nil {
-        return nil, err
-    }
+	cluster := &shipa.Cluster{}
+	err = json.Unmarshal(rawJson, cluster)
+	if err != nil {
+		return nil, err
+	}
 
-    if frameworks != nil {
-        if cluster.Resources == nil {
-            cluster.Resources = &shipa.ClusterResources{}
-        }
-        cluster.Resources.Frameworks = frameworks
-    }
+	if frameworks != nil {
+		if cluster.Resources == nil {
+			cluster.Resources = &shipa.ClusterResources{}
+		}
+		cluster.Resources.Frameworks = frameworks
+	}
 
-    if cluster.Endpoint != nil {
-        cluster.Endpoint.Token = useFileOrValue(cluster.Endpoint.Token)
-        cluster.Endpoint.Certificate = useFileOrValue(cluster.Endpoint.Certificate)
-        cluster.Endpoint.ClientCertificate = useFileOrValue(cluster.Endpoint.ClientCertificate)
-        cluster.Endpoint.ClientKey = useFileOrValue(cluster.Endpoint.ClientKey)
-    }
+	if cluster.Endpoint != nil {
+		cluster.Endpoint.Token = useFileOrValue(cluster.Endpoint.Token)
+		cluster.Endpoint.Certificate = useFileOrValue(cluster.Endpoint.Certificate)
+		cluster.Endpoint.ClientCertificate = useFileOrValue(cluster.Endpoint.ClientCertificate)
+		cluster.Endpoint.ClientKey = useFileOrValue(cluster.Endpoint.ClientKey)
+	}
 
-    return cluster, nil
+	return cluster, nil
 }
 
 func useFileOrValue(value string) string {
-    if value == "" {
-        return ""
-    }
+	if value == "" {
+		return ""
+	}
 
-    data, err := readFile(value)
-    if err != nil {
-        return value
-    }
-    return string(data)
+	data, err := readFile(value)
+	if err != nil {
+		return value
+	}
+	return strings.Trim(string(data), " \n\t")
 }
 
 func readFile(path string) ([]byte, error) {
-    if _, err := os.Stat(path); err != nil {
-        return nil, fmt.Errorf("invalid file path: %v", err)
-    }
+	if _, err := os.Stat(path); err != nil {
+		return nil, fmt.Errorf("invalid file path: %v", err)
+	}
 
-    bytes, err := ioutil.ReadFile(path)
-    if err != nil {
-        return nil, fmt.Errorf("failed to read file: %v", err)
-    }
+	bytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read file: %v", err)
+	}
 
-    return bytes, nil
+	return bytes, nil
 }
 
 // ClusterEndpoint - part of Cluster object
 type ClusterEndpoint struct {
-    Addresses         []string `json:"addresses,omitempty" yaml:"addresses,omitempty"`
-    Certificate       string   `json:"caCert,omitempty" yaml:"caCert,omitempty"`
-    ClientCertificate string   `json:"clientCert,omitempty" yaml:"clientCert,omitempty"`
-    ClientKey         string   `json:"clientKey,omitempty" yaml:"clientKey,omitempty"`
-    Token             string   `json:"token,omitempty" yaml:"token,omitempty"`
-    Username          string   `json:"username,omitempty" yaml:"username,omitempty"`
-    Password          string   `json:"password,omitempty" yaml:"password,omitempty"`
+	Addresses         []string `json:"addresses,omitempty" yaml:"addresses,omitempty"`
+	Certificate       string   `json:"caCert,omitempty" yaml:"caCert,omitempty"`
+	ClientCertificate string   `json:"clientCert,omitempty" yaml:"clientCert,omitempty"`
+	ClientKey         string   `json:"clientKey,omitempty" yaml:"clientKey,omitempty"`
+	Token             string   `json:"token,omitempty" yaml:"token,omitempty"`
+	Username          string   `json:"username,omitempty" yaml:"username,omitempty"`
+	Password          string   `json:"password,omitempty" yaml:"password,omitempty"`
 }
 
 // ClusterResources - part of Cluster object
 type ClusterResources struct {
-    Frameworks         *Framework         `json:"frameworks,omitempty" yaml:"frameworks,omitempty"`
-    IngressControllers []*IngressController `json:"ingressControllers,omitempty" yaml:"ingressControllers,omitempty"`
+	Frameworks         *Framework           `json:"frameworks,omitempty" yaml:"frameworks,omitempty"`
+	IngressControllers []*IngressController `json:"ingressControllers,omitempty" yaml:"ingressControllers,omitempty"`
 }
 
 // IngressController - part of ClusterResources object
 type IngressController struct {
-    IngressIP     string `json:"ingressIp,omitempty" yaml:"ingressIp,omitempty"`
-    ServiceType   string `json:"serviceType,omitempty" yaml:"serviceType,omitempty"`
-    Type          string `json:"type,omitempty" yaml:"type,omitempty"`
-    HTTPPort      int64  `json:"httpPort,omitempty" yaml:"httpPort,omitempty"`
-    HTTPSPort     int64  `json:"httpsPort,omitempty" yaml:"httpsPort,omitempty"`
-    ProtectedPort int64  `json:"protectedPort,omitempty" yaml:"protectedPort,omitempty"`
-    Debug         bool   `json:"debug" yaml:"debug"`
-    AcmeEmail     string `json:"acmeEmail,omitempty" yaml:"acmeEmail,omitempty"`
-    AcmeServer    string `json:"acmeServer,omitempty" yaml:"acmeServer,omitempty"`
+	IngressIP     string `json:"ingressIp,omitempty" yaml:"ingressIp,omitempty"`
+	ServiceType   string `json:"serviceType,omitempty" yaml:"serviceType,omitempty"`
+	Type          string `json:"type,omitempty" yaml:"type,omitempty"`
+	HTTPPort      int64  `json:"httpPort,omitempty" yaml:"httpPort,omitempty"`
+	HTTPSPort     int64  `json:"httpsPort,omitempty" yaml:"httpsPort,omitempty"`
+	ProtectedPort int64  `json:"protectedPort,omitempty" yaml:"protectedPort,omitempty"`
+	Debug         bool   `json:"debug" yaml:"debug"`
+	AcmeEmail     string `json:"acmeEmail,omitempty" yaml:"acmeEmail,omitempty"`
+	AcmeServer    string `json:"acmeServer,omitempty" yaml:"acmeServer,omitempty"`
 }
 
 // Framework - part of ClusterResources object
 type Framework struct {
-    Name []string `json:"name,omitempty" yaml:"name,omitempty"`
+	Name []string `json:"name,omitempty" yaml:"name,omitempty"`
 }
